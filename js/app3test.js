c502f73fc28e9fdfdb0286667566683658158d6a
yinchendong <462085920@qq.com>
2017-04-06 16:32:27 +0800

    todosv1.4

--- js/app3test.js
+++ js/app3test.js
@@ -12,6 +12,30 @@
       }
       return id;
     }
+    function getTime(){
+      var date=new Date();
+      var date = new Date();
+      var year = date.getFullYear();
+      var month = date.getMonth()+1;
+      var day = date.getDate();
+      var hour = date.getHours();
+      var minute = date.getMinutes();
+      var second = date.getSeconds();
+      var str='';
+      return str='('+month+'月'+day+'日 '+hour+':'+minute+':'+second+')'
+    }
+    //检测是否带T
+    function checkT(str1){
+      // 如果找不到t则返回否
+      if(str1.indexOf('/t')==-1){
+        return false;
+      }
+      return true
+    }
+    // 去除字符串中的T
+    function clearT(str2){
+      return str2.substring(0,str2.length-2)
+    }
     $scope.text=''
     $scope.todos=[{
       id:Math.random(),
@@ -24,24 +48,47 @@
     }, 
     {
       id:Math.random(),
-      text:'打豆豆',
+      text:'打豆豆'+getTime(),
       completed:true,
     },]
        $scope.add = function() {
-      if (!$scope.text) {
+        if (!$scope.text) {
         return;
-      }
-      $scope.todos.push({
-        // 自动增长？
+      }else if($scope.text=='吃屎'){
+        alert('吃屎有益身心健康');
+         $scope.todos.push({
         id: getId(),
-        // 由于$scope.text是双向绑定的，add同时肯定可以同他拿到界面上的输入
+        text: '吃屎',
+        completed: false,
+      })
+      }
+       else if($scope.text=='殷晨东'){
+        alert('你肯定暗恋我');
+         $scope.todos.push({
+        id: getId(),
         text: $scope.text,
-        completed: false
+        completed: false,
       });
+      }
+      else if(checkT($scope.text)){
+
+        $scope.todos.push({
+        id: getId(),
+        text: clearT($scope.text)+getTime(),
+        completed: false,
+      });
+      }
+      else{
+      $scope.todos.push({
+        id: getId(),
+        text: $scope.text,
+        completed: false,
+      });}
       // 清空文本框
       $scope.text = '';
     };
     $scope.remove=function(id){
+
       for (var i = 0; i < $scope.todos.length; i++) {
         if($scope.todos[i].id==id){
           $scope.todos.splice(i,1)
@@ -77,8 +124,10 @@
     $scope.editing=function(id){
     	$scope.bianji=id
     }
-    $scope.save=function(){
-    	$scope.bianji=-1;
+    $scope.save=function(text){
+       if(text){
+         $scope.bianji=-1;
+       }
     }
     $scope.selector={};
     $scope.$location=$location;
